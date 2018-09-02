8c166cc5dcdd9354699f1bc55830f6751b03952f
Aldon <aisenberg@freightview.com>
2018-09-02 14:53:19 -0500

    handle match status

--- src/views/MatchUnderway.vue
+++ src/views/MatchUnderway.vue
@@ -7,7 +7,7 @@
       <h2>There should be a component here that has a few different tabs:</h2>
       <ul>
         <li>Match History (graph)</li>
-        <li>Wrestler's info</li>
+        <li>Wrestler's info (both on web, tab per wrestler on mobile)</li>
         <li>other wrestler (on mobile)</li>
         <li>Current Bracket</li>
         <li>Stats</li>
@@ -17,34 +17,33 @@
 
     <div class="score">
       <h2>Here is where the score should be</h2>
-      <p>BLUE   |    RED</p>
+      <p>BLUE: {{blueScore}}   |    RED: {{redScore}}</p>
     </div>
 
 
     <div class="matchActions">
-      
-      <ul class ="greenActions">
-        <li>Green Takedown</li>
-        <li>Green Escape</li>
-        <li>Green Reversal</li>
-        <li>Green Nearfall2</li>
-        <li>Green Nearfall3</li>
-        <li>Green Nearfall4</li>
-        <li>Green Pin</li>
-        <li>Green Penalty</li>
+
+      <ul class ="blueActions">
+        <li v-show="neutral">blue Takedown</li>
+        <li v-show="redControl">blue Escape</li>
+        <li v-show="redControl">blue Reversal</li>
+        <li v-show="blueControl">blue Nearfall2</li>
+        <li v-show="blueControl">blue Nearfall3</li>
+        <li v-show="blueControl">blue Nearfall4</li>
+        <li v-show="blueControl || redControl">blue Pin</li>
+        <li>blue Penalty</li>
       </ul>
 
       <ul class ="redActions">
-        <li>Red Takedown</li>
-        <li>Red Escape</li>
-        <li>Red Reversal</li>
-        <li>Red Nearfall2</li>
-        <li>Red Nearfall3</li>
-        <li>Red Nearfall4</li>
-        <li>Red Pin</li>
+        <li v-show="neutral">Red Takedown</li>
+        <li v-show="blueControl">Red Escape</li>
+        <li v-show="blueControl">Red Reversal</li>
+        <li v-show="redControl">Red Nearfall2</li>
+        <li v-show="redControl">Red Nearfall3</li>
+        <li v-show="redControl">Red Nearfall4</li>
+        <li v-show="redControl || blueControl">Red Pin</li>
         <li>Red Penalty</li>
       </ul>
-
     </div>
 
 
@@ -53,6 +52,16 @@
 
 <script>
 export default {
+  data : function () {
+    return {
+      blueScore: 0,
+      redScore: 0,
+      neutral: false,
+      blueControl: true,
+      redControl: false,
+      pause: false
+    }
+  }
 }
 </script>
 
