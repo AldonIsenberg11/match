caf5c4f1ad88bd9f7523d4905cad44d6adf2c29b
Aldon <aisenberg@freightview.com>
2018-09-01 23:43:49 -0500

    get to Match Page

--- src/router.js
+++ src/router.js
@@ -12,12 +12,20 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/match',
+      name: 'match',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "match" */ './views/Match.vue')
+    },
+    {
+      path: '/matchUnderway',
+      name: 'matchUnderway',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "matchUnderway" */ './views/MatchUnderway.vue')
     }
   ]
 })
