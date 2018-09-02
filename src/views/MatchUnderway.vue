a130774ee2e31119d569237b59a35089020bd5c3
Aldon <aisenberg@freightview.com>
2018-09-02 12:15:33 -0500

    add matchUnderway initial structure

--- src/views/MatchUnderway.vue
+++ src/views/MatchUnderway.vue
@@ -1,5 +1,54 @@
-<template lang="html">
-  <h1>Match Underway</h1>
+<template>
+  <div class="matchUnderway">
+    <h1>Match Underway</h1>
+
+
+    <div class="matchStats">
+      <h2>There should be a component here that has a few different tabs:</h2>
+      <ul>
+        <li>Match History (graph)</li>
+        <li>Wrestler's info</li>
+        <li>other wrestler (on mobile)</li>
+        <li>Current Bracket</li>
+        <li>Stats</li>
+      </ul>
+    </div>
+
+
+    <div class="score">
+      <h2>Here is where the score should be</h2>
+      <p>BLUE   |    RED</p>
+    </div>
+
+
+    <div class="matchActions">
+      
+      <ul class ="greenActions">
+        <li>Green Takedown</li>
+        <li>Green Escape</li>
+        <li>Green Reversal</li>
+        <li>Green Nearfall2</li>
+        <li>Green Nearfall3</li>
+        <li>Green Nearfall4</li>
+        <li>Green Pin</li>
+        <li>Green Penalty</li>
+      </ul>
+
+      <ul class ="redActions">
+        <li>Red Takedown</li>
+        <li>Red Escape</li>
+        <li>Red Reversal</li>
+        <li>Red Nearfall2</li>
+        <li>Red Nearfall3</li>
+        <li>Red Nearfall4</li>
+        <li>Red Pin</li>
+        <li>Red Penalty</li>
+      </ul>
+
+    </div>
+
+
+  </div>
 </template>
 
 <script>
