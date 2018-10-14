e1417406446cbe54a9dad34ff4688dc93d4b89cc
Aldon <aisenberg@freightview.com>
2018-10-14 14:56:58 -0500

    update with colors

--- src/server/app.js
+++ src/server/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 8081
+const colors = require('colors')
 
 const db = require('./services/shared-db.js')
 
@@ -8,4 +9,4 @@ db.connect()
 
 app.get('/', (req, res) => res.send('HelloWorld!'))
 
-app.listen(port, () => console.log (`Example app listening on port ${port}`))
+app.listen(port, () => console.log (`Example app listening on port ${port}`.cyan.bgBlue))
--- src/server/services/shared-db.js
+++ src/server/services/shared-db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
+const colors = require('colors')
 
 let handleFailure = function() {
-  console.log("There was an error");
+  console.log("There was an error"..bold.red.bgWhite);
   return process.exit(1)
 }
 
@@ -12,8 +13,8 @@ module.exports = {
 
     mongoose.connection.on("error", handleFailure)
     mongoose.connection.on("open", function() {
-      console.log(`MongoDB connected to[${connection.db.databaseName}]`)
+      console.log(`MongoDB connected to[${connection.db.databaseName}]`.grey.bgRed)
     })
-    mongoose.connect('mongodb://localhost:27017/wrestle-db')
+    mongoose.connect('mongodb://localhost:27017/wrestle-db', { useNewUrlParser : true } )
   }
 }
